25f61ff63c958644f3dc74bfd94f827e46f91dad
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-24 23:53:43 +0500

    Working..

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./src/db/index.js";
 
 dotenv.config();
@@ -8,21 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.get("/",(req , res) => {
-    res.send("It's FINAL HACKATHON");
+app.get("/", (req, res) => {
+  res.send("It's FINAL HACKATHON");
 });
-
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT , () => {
-        console.log(`Server ir running at the port: ${process.env.PORT}`);
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️  Server ir running at the port: ${process.env.PORT}`);
     });
-})
-.catch((error) => {
- console.log("❌ MONGO DB connection failed ❌" , error);
-});
-
-
-
+  })
+  .catch((error) => {
+    console.log("❌ MONGO DB connection failed ❌", error);
+  });
