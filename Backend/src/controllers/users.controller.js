28bfefa7ed50de5ac82a4be7189918a504bb8195
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-25 02:29:15 +0500

    Working...

--- Backend/src/controllers/users.controller.js
+++ Backend/src/controllers/users.controller.js
@@ -5,14 +5,27 @@ import bcrypt from "bcrypt";
 // To generate access token
 const generateAccessToken = (user) => {
     return jwt.sign({ email: user.email }, process.env.ACCESS_JWT_SECRET, {
-      expiresIn: "6h",
+      expiresIn: "6h",  //change a/c to given task
     });
   };
 
-  // To generate refresh token
 
+// To generate refresh token
 const generateRefreshToken = (user) => {
     return jwt.sign({ email: user.email }, process.env.REFRESH_JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: "7d",  //change a/c to given task
     });
   };
+
+
+// To register the User
+const registerUser = async (req, res) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      res.status(400).json({
+        message: "You must enter email & password",
+      });
+      return;
+    }
+    
