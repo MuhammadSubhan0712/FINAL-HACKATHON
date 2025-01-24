850da66ea9380b635e0a9935f2b574169f9ef0ba
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-25 02:31:02 +0500

    Working...

--- Backend/src/controllers/users.controller.js
+++ Backend/src/controllers/users.controller.js
@@ -4,22 +4,48 @@ import bcrypt from "bcrypt";
 
 // To generate access token
 const generateAccessToken = (user) => {
-    return jwt.sign({ email: user.email }, process.env.ACCESS_JWT_SECRET, {
-      expiresIn: "6h",  //change a/c to given task
-    });
-  };
-
+  return jwt.sign({ email: user.email }, process.env.ACCESS_JWT_SECRET, {
+    expiresIn: "6h", //change a/c to given task
+  });
+};
 
 // To generate refresh token
 const generateRefreshToken = (user) => {
-    return jwt.sign({ email: user.email }, process.env.REFRESH_JWT_SECRET, {
-      expiresIn: "7d",  //change a/c to given task
-    });
-  };
-
+  return jwt.sign({ email: user.email }, process.env.REFRESH_JWT_SECRET, {
+    expiresIn: "7d", //change a/c to given task
+  });
+};
 
 // To register the User
 const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400).json({
+      message: "You must enter username email & password",
+    });
+    return;
+  }
+  const user = await User.findOne({ email: email });
+  if (user) {
+    res.status(401).json({
+      message: "User Already Exist",
+    });
+  }
+  const createUser = await User.create({
+    username,
+    email,
+    password,
+  });
+  res.json({
+    message: "User Registered Successfully",
+    data: createUser,
+  });
+};
+
+
+// To Login User
+const loginUser = async (req, res) => {
     const { email, password } = req.body;
   
     if (!email || !password) {
@@ -28,9 +54,4 @@ const registerUser = async (req, res) => {
       });
       return;
     }
-    const user = await User.findOne({ email: email });
-  if (user) {
-    res.status(401).json({
-      message: "User Already Exist",
-    });
-  }
+  
